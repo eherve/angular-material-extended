65e64e3c0629985c0cd1720e41de950a7508d063
eherve <eric.herve.95@gmail.com>
2024-12-22 14:39:13 +0100

    v19

--- projects/datetime-picker/src/lib/datetime-picker.component.ts
+++ projects/datetime-picker/src/lib/datetime-picker.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  imports: [],
-  standalone: true,
-  selector: 'ngx-mat-datetime-picker',
-  templateUrl: 'datetime-picker.component.html',
-  styleUrls: ['datetime-picker.component.scss'],
+    imports: [],
+    selector: 'ngx-mat-datetime-picker',
+    templateUrl: 'datetime-picker.component.html',
+    styleUrls: ['datetime-picker.component.scss']
 })
 export class DatetimePickerComponent {}
