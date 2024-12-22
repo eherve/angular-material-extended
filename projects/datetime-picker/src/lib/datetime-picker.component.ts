313769f9b654174344aa6ec8f9ed0fede0b13dff
eherve <eric.herve.95@gmail.com>
2024-12-22 14:23:07 +0100

    add datetime picker

---
+++ projects/datetime-picker/src/lib/datetime-picker.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  standalone: true,
+  selector: 'lib-datetime-picker',
+  templateUrl: 'datetime-picker.component.html',
+  styleUrls: ['datetime-picker.component.scss'],
+})
+export class DatetimePickerComponent {}
