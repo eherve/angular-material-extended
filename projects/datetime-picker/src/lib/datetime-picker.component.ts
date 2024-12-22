783afbdde88c33a74930a5aa4b8eb06dce851fd1
eherve <eric.herve.95@gmail.com>
2024-12-22 14:34:23 +0100

    add test app

--- projects/datetime-picker/src/lib/datetime-picker.component.ts
+++ projects/datetime-picker/src/lib/datetime-picker.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   imports: [],
   standalone: true,
-  selector: 'lib-datetime-picker',
+  selector: 'ngx-mat-datetime-picker',
   templateUrl: 'datetime-picker.component.html',
   styleUrls: ['datetime-picker.component.scss'],
 })
